ba71f894453e66dad253712444fcc2f5d3eaa37e
janberjanson <122324036+janberjanson@users.noreply.github.com>
2023-01-10 07:34:20 +0800

    Test file

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>untitled</title>
+	</head>
+	<body>
+<h1><h1>
+
+<?php
+$name="test";
+echo $name .' me <br>'; 
+echo "$name me<br>"; 
+echo '$name me<br>'; 
+echo "{$name}me<br>"; 
+echo "[$name]me<br>"; 
+echo ucfirst($name); 
+
+
+
+define("Value_of_Pi",3.14); 
+echo "PI Value: " .Value_of_Pi
+
+?>
+	</body>
+</html>
